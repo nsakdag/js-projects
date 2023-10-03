f65aeb4728027f4f76765807e9861a9d23812170
nsakdag <nsakdag88@gmail.com>
2023-10-03 21:52:51 +0200

    prompt eklendi

--- calculater/index.js
+++ calculater/index.js
@@ -1,38 +1,27 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = Number(prompt("lütfen birinci sayıyı giriniz:"));
+let num2 = Number(prompt("lütfen ikinci sayıyı giriniz:"));
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
 
-let sumEL = document.getElementById("sum-el") 
-
+let sumEL = document.getElementById("sum-el");
 
 function add() {
-   let added = (num1 + num2)
-   sumEL.textContent = "Sum :" + added
-
-    
+  let added = num1 + num2;
+  sumEL.textContent = "Sum :" + added;
 }
 
 function subtract() {
-    let subtracted = (num1 - num2)
-    sumEL.textContent = "Sum :" + subtracted
-   
+  let subtracted = num1 - num2;
+  sumEL.textContent = "Sum :" + subtracted;
 }
 function divide() {
-    let divided = (num1 / num2)
-    sumEL.textContent = "Sum :" + divided
-    console.log(sumEL)
+  let divided = (num1 / num2).toFixed(2);
+  sumEL.textContent = "Sum :" + divided;
+  console.log(sumEL);
 }
 
 function multiply() {
-    let multiplyed = (num1 * num2)
-    sumEL.textContent = "Sum :" + multiplyed
-    console.log(sumEL)
+  let multiplyed = num1 * num2;
+  sumEL.textContent = "Sum :" + multiplyed;
+  console.log(sumEL);
 }
-
-
-
-
-
-
-
